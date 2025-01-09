9c9e29ac4245895b95bf4c1a88adf8779ce8c857
dai main pc <aaa@aaa.com>
2025-01-09 22:59:46 +0900

    update archive

--- bin/x64/plugins/cyber_engine_tweaks/mods/InsideStation/init.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/InsideStation/init.lua
@@ -15,7 +15,7 @@ local Debug = require('Debug/debug.lua')
 
 InsideStation = {
 	description = "Inside The Station",
-	version = "1.0.3",
+	version = "1.0.4",
     is_debug_mode = false,
     -- version check
     cet_required_version = 32.2, -- 1.32.2
